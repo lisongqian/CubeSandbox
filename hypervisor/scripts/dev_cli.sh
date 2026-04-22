988540355fdf9f2f9c80a608df6f0e188122775b
Songqian Li <sionli@tencent.com>
2026-04-22 15:42:32 +0800

    ci: enable integration tests for hypervisor
    Signed-off-by: Songqian Li <sionli@tencent.com>

--- hypervisor/scripts/dev_cli.sh
+++ hypervisor/scripts/dev_cli.sh
@@ -397,6 +397,7 @@ cmd_tests() {
             --device $exported_device \
             --device /dev/net/tun \
             --cap-add net_admin \
+            --volume /dev:/dev \
             --volume "$CLH_ROOT_DIR:$CTR_CLH_ROOT_DIR" $exported_volumes \
             --env BUILD_TARGET="$target" \
             "$CTR_IMAGE" \
